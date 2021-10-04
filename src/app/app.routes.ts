4b18bc44dedb3b403ceb6d867e7dcafcb96ad0f5
santiago cortes <jeissoncortes02@outlook.com>
2021-10-04 11:09:17 -0500

    creacion del route module para manejar rutras

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from "@angular/router";
+
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+  ];
+export const app_routing = RouterModule.forRoot(routes);
