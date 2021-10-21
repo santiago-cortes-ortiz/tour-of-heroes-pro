d9a4494ce441d730f30e9594d7d56c8a9180e725
santiago cortes <jeissoncortes02@outlook.com>
2021-10-21 14:22:34 -0500

    creacion del componente del buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 
 import { HeroesService } from './services/heroes.service';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +23,8 @@ import { HeroeComponent } from './components/heroe/heroe.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
-    HeroeComponent
+    HeroeComponent,
+    BuscadorComponent
     
   ],
   imports: [
